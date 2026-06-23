23d6e05ea4d31886ff9ff36da706e0c104c7fab7
Cacow69 <cmfcursino@gmail.com>
2026-06-23 20:46:44 -0300

    style: Added logo

--- src/components/RightMenu.tsx
+++ src/components/RightMenu.tsx
@@ -1,9 +1,18 @@
 import { Settings } from "lucide-react"
+import Logo from "../static/img/logo-light.png"
 
 function RightMenu() {
   return (
     <div className="min-h-screen w-full md:w-1/4 lg:w-1/5 flex flex-col items-center gap-4 p-4">
-      <div className="w-full max-w-xs flex-1 min-h-64 bg-[#121212] rounded-lg"></div>
+      <div className="w-full max-w-xs h-50 flex flex-col items-start justify-center p-5 gap-5">
+        <img
+          src={Logo}
+          alt="Logo"
+          className="w-[300px] h-[300px] object-contain"
+        />
+      </div>
+
+      <div className="w-full max-w-xs flex-1 min-h-64 rounded-lg" />
 
       <div className="w-full max-w-xs h-20 bg-[#121212] rounded-lg flex flex-col items-start justify-center p-5 gap-5">
         <div className="w-full flex items-center gap-2 text-white text-lg font-bold cursor-pointer transition-colors hover:bg-gray-800 p-2 rounded-lg">
